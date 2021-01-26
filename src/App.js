f687b0132c309210db7f478848d07694062a342d
Justin Carter <justin@vidpresso.com>
2021-01-25 23:53:52 -0500

    cors!

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import {distanceTo} from 'geolocation-utils';
 import './App.css';
 import { useEffect, useState } from 'react';
 
-const API_URL = "/api/siri/stop-monitoring.json";
+// const API_URL = "https://bustime.mta.info/api/siri/stop-monitoring.json";
+const API_URL = "/.netlify/functions/node-fetch";
 const york = { //401916,YORK AV/E 91 ST,40.778847,-73.945062
   monitoringRef: 401916,
   name: "YORK AV/E 91 ST",
@@ -60,17 +61,13 @@ function App() {
   }
 
   const getData = () => {
-    fetch(`${API_URL}?key=${process.env.REACT_APP_MTA_BUS_TIME_API_KEY}&MonitoringRef=${location.monitoringRef}`, {
-      method: "GET",
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      }
-    })
+    fetch(`${API_URL}?key=${process.env.REACT_APP_MTA_BUS_TIME_API_KEY}&MonitoringRef=${location.monitoringRef}`)
     .then(response => response.json())
     .then(data => {
       const stopVisits = data.Siri?.ServiceDelivery?.StopMonitoringDelivery?.[0]?.MonitoredStopVisit || [];
       // console.log(data.Siri)
       // console.log(stopVisits)
+      // console.log(data)
       const busArr = []
       for (const visit of stopVisits) {
         const vehicle = visit.MonitoredVehicleJourney;
