c0975a240a29925e7c31b91e45ff1e1f088eac7d
Justin Carter <justin@vidpresso.com>
2021-01-30 16:35:28 -0500

    test8

--- src/TempMap.js
+++ src/TempMap.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl-unminified';
 import './Temp.css';
 
 mapboxgl.accessToken =
